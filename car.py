b3a25e6474a5b943d7471ea5714109b5b68389db
Piotr Glazer <piotrglazer@MacBook-Air-Piotr.local>
2018-09-30 09:34:55 +0200

    car z błędami

---
+++ car.py
@@ -0,0 +1,12 @@
+car = input('Serwis Mercedesa: ')
+rent = input('Apartament w śródmieściu: ')
+jet = input('Wynajem prywatnego samolotu: ')
+gifts = input('Podarunki: ')
+food = input('Obiady w restauracjach: ')
+staff = input('Personel (służba domowa, kucharz, kierowca, asystent): ')
+guru = input('Osobisty guru i coach: ')
+games = input('Gry komputerowe: ')
+
+total = car+rent+jet+gifts+food+staff+guru+games
+
+print('\nOgółem: ',total)
