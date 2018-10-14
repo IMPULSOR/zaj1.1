f952e31017b76b1703377be31a6f6ecf14b0aaca
Piotr Glazer <piotr.glazer@hotmail.com>
2018-10-14 15:06:24 +0200

    Zadanie 10 dodatkowe

--- zadanie10_dodatkowe.py
+++ zadanie10_dodatkowe.py
@@ -2,27 +2,31 @@ from random import randint
 
 rozmiar = int(input("Podaj rozmiar macierzy: "))
 
-macierzA = []
 
+macierzA = []
+print(range(rozmiar))
 for element in range(rozmiar):
     wersA = []
+    macierzA.append(wersA)
     for liczba in range(rozmiar):
         wersA.append(randint(1,100))
-        macierzA.append(wersA)
+
 print(macierzA)
 
 macierzB = []
 for element in range(rozmiar):
     wersB = []
+    macierzB.append(wersB)
     for liczba in range(rozmiar):
         wersB.append(randint(1,100))
-        macierzB.append(wersB)
+
 print(macierzB)
 
 macierzC = []
 for i in range(rozmiar):
     for j in range(rozmiar):
         macierzC.append(macierzA[i][j]+macierzB[i][j])
+
 print(macierzC)
 
 
